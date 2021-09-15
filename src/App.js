7f9be4062b378c4a5f36991fc259c4cb7e707f29
jhlee <hifive33@naver.com>
2021-09-15 15:12:05 +0900

    #3 STATE

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
-import PropTypes from "prop-types";
+import React from "react";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image: "https://www.seriouseats.com/thmb/mANmBYm-pJfwrwHksChVWur6w9w=/1500x1125/filters:fill(auto,1)/20210527-baechu-kimchi-vicky-wasik-seriouseats-seriouseats-3-18a2d6d7d1d74a7a82cb13ed350218be.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "Ramen",
-    image: "https://motto-jp.com/media/wp-content/uploads/2020/03/eyecatch-11.jpeg",
-    rating: 4.2
-  },
-  {
-    id: 3,
-    name: "Samgyeopsal",
-    image: "http://www.gildedgingerbread.com/wp-content/uploads/2017/08/Samgyeopsal-1.jpg",
-    rating: 4.8
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false })
+    }, 3000);
   }
-];
-
-function Food({ name, picture, rating }) {
-  return(
-  <div>
-    <h3>I like {name}</h3>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name} width="150" />
-  </div>);
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />)}
-    </div>
-  );
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>
+        {isLoading ? "Loading..." : "We are ready"}
+      </div>
+    );
+  }
 }
 
 export default App;
